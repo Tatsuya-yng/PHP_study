741d9255668510372b31a80958f549a52565c047
”名前” <>
2019-11-08 15:28:12 +0000

    ０３PHP再提出

--- practice03課題.php
+++ practice03課題.php
@@ -27,3 +27,18 @@ foreach($fruits as $fruits){
     echo "\n";
 }
 
+
+
+/* for文の始めの値を定義する*/
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++) {
+    if($i % 5 == 0) {
+    echo $i;
+    echo "\n";
+}
+// 5で割り切れたら{}内を実行する
+}
+    
